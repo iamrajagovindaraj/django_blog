5d4e471549908dcb453dbb6f10c5a1976389557a
Raja Govindaraj <iamrajagovindaraj@gmail.com>
2020-08-21 23:09:45 +0530

    Django Templates - skeleton blog

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Raja',
+        'title': 'Blog post1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2020',
+    },
+    {
+        'author': 'Corey',
+        'title': 'Blog post2',
+        'content': 'First post content',
+        'date_posted': 'August 28, 2020',
+    },
+    {
+        'author': 'Michel',
+        'title': 'Blog post3',
+        'content': 'Third post content',
+        'date_posted': 'August 21, 2020',
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1> About </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
